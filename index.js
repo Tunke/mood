7cf299498f4fa11b625f49ca21048c7df1c35aef
s085178 <s085178@ap.be>
2016-03-25 14:43:16 +0000

    Initail Commit

---
+++ index.js
@@ -0,0 +1,11 @@
+var express = require('express'),
+    app = express();
+    
+app.use('/', express.static(__dirname + '/public'));
+
+app.get('/', function(request, response) {
+   response.sendFile(__dirname + '/simple-app/public/index.html'); 
+});
+
+app.listen(8080);
+
